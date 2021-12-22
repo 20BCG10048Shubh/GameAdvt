a5a0a44b9865796da2183d98af8dd2d524642ddd
20BCG10048Shubh <96327894+20BCG10048Shubh@users.noreply.github.com>
2021-12-22 09:27:01 +0530

    Add files via upload

---
+++ abc.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class abc : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0,0,Time.deltaTime);
+        transform.Rotate(0f, -0.005f, -0.015f);
+        Invoke("moveForward", 8f);
+
+    }
+    void moveForward()
+    {
+        transform.Translate(0, 0, Time.deltaTime * 4);
+    }
+}
